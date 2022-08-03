8dfb471c8447419acfcb92a35689a76bfad0fbe8
Carlos Jaime <jaime_andrek@hotmail.com>
2022-08-02 21:03:30 -0300

    Criando meu proprio component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-
+    <h1>Ola curso</h1>
+    <Cliente/>
   </div>
 </template>
 
 <script>
+import Cliente from './components/Cliente'
+export default {
+   name: 'App',
+   components: {
+      Cliente
+   }
+}
 
 </script>
 
 <style>
+
 
 </style>
