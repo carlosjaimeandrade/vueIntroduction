157430c36482823e891d7d6de5da792201ccf202
Carlos Jaime <jaime_andrek@hotmail.com>
2022-08-03 22:36:48 -0300

    two data binding

--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,20 +1,21 @@
 <template>
     <div>
-        
+
         <h1>{{ email }}</h1>
         <h2>Cliente {{ nome }} idade {{ idade }}</h2>
 
         <input :value="nome" type="text">
-        
+        <hr>
+        <input v-model="nome" type="text">
     </div>
 </template>
 
 <script>
 export default {
-    data(){
+    data() {
         return {
             nome: "Carlos",
-            idade: 25, 
+            idade: 25,
             email: "jaime_andrek@hotmail.com"
         }
     }
@@ -22,7 +23,7 @@ export default {
 </script>
 
 <style scoped>
-.client{
+.client {
     background-color: rgba(92, 83, 83, 0.24);
 }
 </style>
