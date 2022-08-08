83cb35953c6d36039bbea86c3d6a9a24cc0dd37e
Carlos Jaime <jaime_andrek@hotmail.com>
2022-08-08 19:59:59 -0300

    v-for

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,13 @@
     <input v-model="codigoPai" type="text">
     <Produto :tipo="true" :showQuantidade="false" :codigo="codigoPai" nome="Pendrive" quantidade="20" />
     <Produto :tipo="false" :showQuantidade="true" codigo="AS5442" nome="CD" quantidade="2" />
+
+    <div v-for="(produto, index) in produtos" :key="produto.id">
+      <p>{{index}}</p>
+      <input v-model="produto.nome" type="text">
+      <Produto :tipo="true" :showQuantidade="true" :codigo="produto.codigo" :nome="produto.nome" quantidade="20" />
+    </div>
+
   </div>
 </template>
 
@@ -13,9 +20,21 @@ import Produto from './components/Produto'
 
 export default {
   name: 'App',
-  data(){
+  data() {
     return {
-      codigoPai: "55458"
+      codigoPai: "55458",
+      produtos: [
+        {
+          id: 1,
+          nome: "Celular",
+          codigo: "44545"
+        },
+        {
+          id: 2,
+          nome: "Pendrive",
+          codigo: "2323"
+        }
+      ],
     }
   },
   components: {
