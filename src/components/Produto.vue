45ea30ecd629de6fbf42c83b057a7bcadc6b4e12
Carlos Jaime <jaime_andrek@hotmail.com>
2022-08-09 21:54:05 -0300

    eventos com vue.js

--- src/components/Produto.vue
+++ src/components/Produto.vue
@@ -6,18 +6,31 @@
             <p v-if="showQuantidade === true">Nome: {{ nome }}</p>
             <p v-else>Escondeu o nome</p>
             <p>Quantidade: {{ quantidade }}</p>
+            <p>{{status}}</p>
+            <button @click="mudarStatus($event)">{{status == true ? "desativar" : "ativar"}}</button>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
+            status: true
+        }
+    },
     props: {
         nome: String,
         quantidade: Number,
         codigo: String,
         showQuantidade: Boolean,
         tipo: Boolean
+    },
+    methods: {
+        mudarStatus: function ($event){
+            console.log($event.target.parentNode.children[0].innerHTML);
+            this.status = !this.status
+        }
     }
 }
 </script>
